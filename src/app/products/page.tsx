1098bd89a4231d544fe4df713798b051bde1e410
jmxor <jamesmrippeth@gmail.com>
2024-05-17 22:22:41 +0100

    Add basic new product modal to products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,81 +1,90 @@
 'use client';
 
-import {Add, Upload} from '@mui/icons-material';
+import ProductDetailsDialog from '@/components/ProductDetailsDialog';
+import {Add} from '@mui/icons-material';
 import {Button} from '@mui/material';
-import {DataGrid, GridColDef, GridRowModesModel, GridRowsProp, GridSlots, GridToolbarContainer} from '@mui/x-data-grid';
+import {
+  DataGrid,
+  GridColDef,
+  GridSlots,
+  GridToolbarColumnsButton,
+  GridToolbarContainer,
+  GridToolbarDensitySelector,
+  GridToolbarFilterButton
+} from '@mui/x-data-grid';
 import {useState} from 'react';
 
-const initialRows: GridRowsProp = [
-  {id: 1, sku: '11H-12462-00', description: '', quantity: 15, minQuantity: 5, price: 32.5, location: '35A'},
-  {id: 2, sku: '1A0-26242-01', description: '', quantity: 6, minQuantity: 5, price: 10.5, location: '24F'},
-  {id: 3, sku: '1RC-15411-01', description: '', quantity: 35, minQuantity: 5, price: 119, location: '64G'},
-  {id: 4, sku: '1RC-27200-00', description: '', quantity: 0, minQuantity: 5, price: 70, location: '5'},
-  {id: 5, sku: '29L-14451-00', description: '', quantity: 23, minQuantity: 5, price: 9.5, location: '5'},
-  {id: 6, sku: '4L0-16111-00-YK', description: '', quantity: 4, minQuantity: 5, price: 32.5, location: '5'},
-  {id: 7, sku: '4L0-21510-00-36', description: '', quantity: 2, minQuantity: 5, price: 10.5, location: '5'},
-  {id: 8, sku: '4L0-23110-00', description: '', quantity: 5, minQuantity: 5, price: 119, location: '5'},
-  {id: 9, sku: '4L026335-00', description: '', quantity: 0, minQuantity: 5, price: 70, location: '5'},
-  {id: 10, sku: '4LO-83310-00', description: '', quantity: 0, minQuantity: 5, price: 9.5, location: '5'},
-
-];
+export interface ProductData {
+  sku: string,
+  description: string
+  quantityStock: number,
+  quantityOrdered: number,
+  quantityWarn: number,
+  price: number
+  location: string,
+}
 
 interface EditToolbarProps {
-  setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
-  setRowModesModel: (
-    newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
-  ) => void;
+  setModalOpen: (state: boolean) => void;
 }
 
 function EditToolbar(props: EditToolbarProps) {
-  const {setRows, setRowModesModel} = props;
-
-  const handleAddProduct = () => {
-    setRows((oldRows) => [...oldRows,]);
-    setRowModesModel((oldModel) => ({...oldModel,}));
-  };
-
+  const {setModalOpen} = props;
   return (
     <GridToolbarContainer>
-      <Button color="primary" startIcon={<Add/>} onClick={handleAddProduct}>
+      <GridToolbarColumnsButton/>
+      <GridToolbarFilterButton/>
+      <GridToolbarDensitySelector/>
+
+      <Button color="primary" size="small" startIcon={<Add/>} sx={{placeSelf: 'end'}}
+              onClick={() => setModalOpen(true)}>
         New product
-      </Button>
-      <Button color="primary" startIcon={<Upload/>} onClick={handleAddProduct}>
-        Import products
       </Button>
     </GridToolbarContainer>
   );
 }
 
 export default function ProductsPage() {
-  const [rows, setRows] = useState(initialRows);
-  const [rowModesModel, setRowModesModel] = useState<GridRowModesModel>({});
+  const [rows, setRows] = useState<ProductData[]>([]);
+  const [modalOpen, setModalOpen] = useState(false);
 
   // TODO add different currencies
   const priceFormatter: GridColDef['valueFormatter'] = (value: number) => `£${value.toFixed(2)}`;
+  const quantityFormatter: GridColDef['valueFormatter'] = (value: number) => `£${value.toFixed(2)}`;
 
   const columns: GridColDef[] = [
     {field: 'sku', headerName: 'SKU', width: 128},
     {field: 'description', headerName: 'Description', flex: 1},
-    {field: 'quantity', headerName: 'Available Quantity', align: 'right', headerAlign: 'right'},
-    {field: 'quantityOrdered', headerName: 'Ordered Quantity', align: 'right', headerAlign: 'right'},
-    {field: 'minQuantity', headerName: 'Min Qty', align: 'right', headerAlign: 'right'},
+    {field: 'quantityStock', headerName: 'Quantity', width: 128, align: 'right', headerAlign: 'right'},
+    {field: 'quantityWarn', headerName: 'Min Qty', align: 'right', headerAlign: 'right'},
     {field: 'price', headerName: 'Price', width: 112, align: 'right', headerAlign: 'right', valueFormatter: priceFormatter},
-    {field: 'location', headerName: 'Location'},
+    {field: 'location', headerName: 'Location', align: 'right', headerAlign: 'right'},
     {field: 'tags', headerName: 'Tags'},
   ];
 
   return (
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      checkboxSelection
-      getRowClassName={(params) => {
-        if (params.row.quantity === 0) return 'MuiDataGrid-rowError';
-        if (params.row.quantity < params.row.minQuantity) return 'MuiDataGrid-rowWarning';
-        return '';
-      }}
-      slots={{toolbar: EditToolbar as GridSlots['toolbar']}}
-      slotProps={{toolbar: {setRows, setRowModesModel}}}
-    />
+    <>
+      <ProductDetailsDialog
+        open={modalOpen}
+        setOpen={setModalOpen}
+        addProduct={(product: ProductData) => setRows(prevRows => [...prevRows, product])}
+      />
+
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        checkboxSelection
+        getRowId={(row: ProductData) => row.sku}
+        getRowClassName={(params) => {
+          if (params.row.quantityStock === 0) return 'MuiDataGrid-rowError';
+          if (params.row.quantityStock < params.row.quantityWarn) return 'MuiDataGrid-rowWarning';
+          return '';
+        }}
+        slots={{toolbar: EditToolbar as GridSlots['toolbar']}}
+        slotProps={{toolbar: {setModalOpen}}}
+        autoHeight
+        // loading
+      />
+    </>
   );
 }
---
+++ src/components/ProductDetailsDialog/index.tsx
@@ -0,0 +1,219 @@
+import {ProductData} from '@/app/products/page';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Grid,
+  InputAdornment,
+  TextField
+} from '@mui/material';
+import {Dispatch, SetStateAction, useState} from 'react';
+
+export default function ProductDetailsDialog({open, setOpen, addProduct}: {
+  open: boolean,
+  setOpen: Dispatch<SetStateAction<boolean>>
+  addProduct: (product: ProductData) => void,
+}) {
+
+  const [formData, setFormData] = useState({
+    sku: '',
+    description: '',
+    quantityStock: '',
+    quantityOrdered: '',
+    quantityWarn: '',
+    price: '',
+    location: ''
+  });
+  const [formErrors, setFormErrors] = useState({
+    sku: Array<string>(),
+    description: Array<string>(),
+    quantityStock: Array<string>(),
+    quantityOrdered: Array<string>(),
+    quantityWarn: Array<string>(),
+    price: Array<string>(),
+    location: Array<string>(),
+  });
+
+  const validateField = (field: string, value: string): string[] => {
+    if (value == '') return ['Required field'];
+    if (['quantityStock', 'quantityOrdered', 'quantityWarn', 'price'].includes(field) && isNaN(+value)) return ['Must be a positive integer'];
+    return [];
+  };
+
+  const clearData = () => {
+    const keys = Object.keys(formData);
+    let emptyFormData = {...formData};
+    let emptyFormErrors = {...formErrors};
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      emptyFormData = {...emptyFormData, [key as keyof typeof formData]: ''};
+      emptyFormErrors = {...emptyFormErrors, [key as keyof typeof formErrors]: []};
+    }
+    setFormData(emptyFormData);
+    setFormErrors(emptyFormErrors);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setFormData({...formData, [name as keyof typeof formData]: value});
+  };
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
+    setFormErrors({...formErrors, [name as keyof typeof formData]: validateField(name, value)});
+  };
+
+  const handleCancel = (e: React.MouseEvent<HTMLElement>) => {
+    clearData();
+    setOpen(false);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+
+    const isValid = Object.values(formErrors).every(x => x.length == 0);
+    if (isValid) {
+      addProduct({
+        sku: formData.sku,
+        description: formData.description,
+        quantityStock: parseInt(formData.quantityStock),
+        quantityOrdered: parseInt(formData.quantityOrdered),
+        quantityWarn: parseInt(formData.quantityWarn),
+        price: parseInt(formData.price),
+        location: formData.location
+      });
+      clearData();
+      setOpen(false);
+    }
+  };
+
+  return (
+    <Dialog open={open} disableRestoreFocus fullWidth>
+      <form onSubmit={handleSubmit} noValidate autoComplete="off">
+        <DialogTitle>New Product</DialogTitle>
+        <DialogContent>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <DialogContentText>
+                Description
+              </DialogContentText>
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                autoFocus
+                fullWidth
+                id="sku"
+                name="sku"
+                label="SKU"
+                value={formData.sku}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={formErrors.sku.length > 0}
+                helperText={formErrors.sku}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                required
+                multiline
+                fullWidth
+                id="description"
+                name="description"
+                label="Description"
+                value={formData.description}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={formErrors.description.length > 0}
+                helperText={formErrors.description}
+              />
+            </Grid>
+
+            <Grid item xs={6}>
+              <TextField
+                required
+                fullWidth
+                id="quantityStock"
+                name="quantityStock"
+                label="Quantity In-Stock"
+                value={formData.quantityStock}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={formErrors.quantityStock.length > 0}
+                helperText={formErrors.quantityStock}
+              />
+            </Grid>
+
+            <Grid item xs={6}>
+              <TextField
+                required
+                fullWidth
+                id="quantityOrdered"
+                name="quantityOrdered"
+                label="Quantity On Order"
+                value={formData.quantityOrdered}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={formErrors.quantityOrdered.length > 0}
+                helperText={formErrors.quantityOrdered}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="quantityWarn"
+                name="quantityWarn"
+                label="Low Stock Warning Level"
+                value={formData.quantityWarn}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={formErrors.quantityWarn.length > 0}
+                helperText={formErrors.quantityWarn}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="price"
+                name="price"
+                label="Price"
+                InputProps={{startAdornment: <InputAdornment position="start">£</InputAdornment>,}}
+                value={formData.price}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={formErrors.price.length > 0}
+                helperText={formErrors.price}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                id="location"
+                name="location"
+                label="Location Code"
+                value={formData.location}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={formErrors.location.length > 0}
+                helperText={formErrors.location}
+              />
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancel} color="error">Discard</Button>
+          <Button type="submit">Create</Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+}
